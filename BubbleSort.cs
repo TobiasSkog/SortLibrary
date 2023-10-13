bca492addc7e35f4161ca87279f876da88570998
Tobias Skog <t_ott_e@hotmail.com>
2023-10-13 02:49:00 +0200

    Complete rework  from intiial testing stage to making the method use Comparison<T> instead and use a Comparer. It takes custom Comparison rules if provided otherwise takes a default null and runs a Comparer<T>.Default.Compare and only checks if list[i]  is bigger than list[i+1] . For numbers no rules are required but for custom objects, strings and other Types a custom rule is prefered to get the expected result.
    A custom comparison for a list of strings where you want to sort them from smallest word to biggest with `.Length` you can use this example:
    `Comparison<string> customStringCompare = (a, b) => a.Length.CompareTo(b.Length);`

--- BubbleSort.cs
+++ BubbleSort.cs
@@ -2,133 +2,44 @@
 {
     public static class BubbleSort
     {
-        private static bool _swapped = true;
-        private static int _n;
-
-        public static List<T> Sort<T>(List<T> list)
+        public static List<T> Sort<T>(List<T> list, Comparison<T> comparison = null)
         {
-            var typeDictionary = new Dictionary<string, List<T>>
+            bool swapped;
+            if (list == null || list.Count < 1)
             {
-                {"int", new List<T>() },
-                {"double", new List<T>() },
-                {"float", new List<T>() },
-                {"char", new List<T>() },
-                {"string", new List<T>() },
-            };
+                return list;
+            }
 
-            foreach (var item in list)
+            do
             {
-                var type = item.GetType();
-                if (typeDictionary.ContainsKey(type.Name))
+                swapped = false;
+                for (int i = 0; i < list.Count - 1; i++)
                 {
-                    typeDictionary[type.Name].Add(item);
+                    if (comparison == null)
+                    {
+                        if (Comparer<T>.Default.Compare(list[i], list[i + 1]) > 0)
+                        {
+                            T temp = list[i];
+                            list[i] = list[i + 1];
+                            list[i + 1] = temp;
+
+                            swapped = true;
+                        }
+                    }
+                    else
+                    {
+                        if (comparison(list[i], list[i + 1]) > 0)
+                        {
+                            T temp = list[i];
+                            list[i] = list[i + 1];
+                            list[i + 1] = temp;
+
+                            swapped = true;
+                        }
+                    }
                 }
-            }
+            } while (swapped);
 
-            foreach (var key in typeDictionary.Keys)
-            {
-                switch (key)
-                {
-                    case "int":
-                        _n = typeDictionary[key].Count;
-                        Bubble(typeDictionary[key] as List<int>);
-                        break;
-                    case "double":
-                        _n = typeDictionary[key].Count;
-                        Bubble(typeDictionary[key] as List<double>);
-                        break;
-                    case "float":
-                        _n = typeDictionary[key].Count;
-                        Bubble(typeDictionary[key] as List<float>);
-                        break;
-                    case "char":
-                        _n = typeDictionary[key].Count;
-                        Bubble(typeDictionary[key] as List<char>);
-                        break;
-                    case "string":
-                        _n = typeDictionary[key].Count;
-                        Bubble(typeDictionary[key] as List<string>);
-                        break;
-                    default:
-                        break;
-                }
-            }
-
-            list.Clear();
-            foreach (var key in typeDictionary.Keys)
-            {
-                list.AddRange(typeDictionary[key]);
-            }
-
-            return list;
-        }
-
-        public static List<int> Bubble(List<int> list)
-        {
-
-            while (_swapped)
-            {
-                _swapped = !_swapped;
-                for (int i = 0; i < _n - 1; i++)
-                {
-
-                }
-            }
-            return list;
-        }
-        public static List<double> Bubble(List<double> list)
-        {
-
-            while (_swapped)
-            {
-                _swapped = !_swapped;
-                for (int i = 0; i < _n - 1; i++)
-                {
-
-                }
-            }
-            return list;
-        }
-
-        public static List<float> Bubble(List<float> list)
-        {
-
-            while (_swapped)
-            {
-                _swapped = !_swapped;
-                for (int i = 0; i < _n - 1; i++)
-                {
-
-                }
-            }
-            return list;
-        }
-
-        public static List<char> Bubble(List<char> list)
-        {
-
-            while (_swapped)
-            {
-                _swapped = !_swapped;
-                for (int i = 0; i < _n - 1; i++)
-                {
-
-                }
-            }
-            return list;
-        }
-
-        public static List<string> Bubble(List<string> list)
-        {
-
-            while (_swapped)
-            {
-                _swapped = !_swapped;
-                for (int i = 0; i < _n - 1; i++)
-                {
-
-                }
-            }
             return list;
         }
     }
